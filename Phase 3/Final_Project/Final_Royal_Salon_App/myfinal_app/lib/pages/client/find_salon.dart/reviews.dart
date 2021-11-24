7e810ca3083952d0876050b7308964dac4bb67b1
Catalyst023 <67686210+Catalyst023@users.noreply.github.com>
2021-11-24 08:37:44 +0200

    Update review

--- Phase 3/Final_Project/Final_Royal_Salon_App/myfinal_app/lib/pages/client/find_salon.dart/reviews.dart
+++ Phase 3/Final_Project/Final_Royal_Salon_App/myfinal_app/lib/pages/client/find_salon.dart/reviews.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:myfinal_app/services/user_service.dart';
+import 'package:provider/src/provider.dart';
 
 class SalonReviews extends StatelessWidget {
-  const SalonReviews({Key? key}) : super(key: key);
+  SalonReviews({Key? key}) : super(key: key);
+
+  final TextEditingController _commentsController = TextEditingController();
+
+  double calcRating(int hygiene, int experience, int hairstyle) {
+    double total = 0.0;
+
+    total = (hygiene + experience + hairstyle) / 3;
+
+    return total;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // String salonname =
+    //     context.read<UserService>().currentUser!.getProperty('salon_name');
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Reviews'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Reviews List',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Card(
+                  child: ListTile(
+                title: Text('Salon Name: '),
+                subtitle: Text('I did not have a good experience with them.'),
+                trailing: Text('3.5'),
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
